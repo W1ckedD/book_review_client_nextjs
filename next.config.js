1062c4376626c82803c93d54de067f884e3210ff
w1ckedd <alirezachoobineh@ymail.com>
2021-07-30 09:11:27 +0430

    initialized app

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  rewrites() {
+    return [
+      {
+        source: '/api2/:path*',
+        destination: 'http://localhost:8000/api2/:path*'
+      }
+    ];
+  }
 }
