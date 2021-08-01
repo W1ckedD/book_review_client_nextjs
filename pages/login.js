68f204ed7a2cee31031d2190dd918bcf0345e6ba
w1ckedd <alirezachoobineh@ymail.com>
2021-08-01 17:23:44 +0430

    added login and register pages

--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,7 @@ import Layout from "@components/Layout/Layout"
 
 export default function login() {
   return (
-    <Layout>
+    <Layout title="Login">
       <h1>Login Page</h1>
     </Layout>
   )
