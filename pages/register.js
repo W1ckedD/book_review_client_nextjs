f33ff6cdaa349f20e52d0bebf5ed2c2aaeebe719
w1ckedd <alirezachoobineh@ymail.com>
2021-08-01 20:22:08 +0430

    created login page elements and styles

---
+++ pages/register.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Layout from "@components/Layout/Layout";
+import styles from "@styles/Login.module.css";
+
+export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (e) => {
+    switch (e.target.name) {
+      case "username":
+        setUsername(e.target.value);
+        break;
+      case "password":
+        setPassword(e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+  return (
+    <Layout title="Login">
+      <div className={styles.center}>
+        <h1>Register</h1>
+        <form className={styles.form}>
+          <div className={styles.formGroup}>
+            <label>Username</label>
+            <input
+              className={styles.formControl}
+              name="username"
+              type="text"
+              onChange={handleChange}
+              value={username}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label>Password</label>
+            <input
+              className={styles.formControl}
+              name="password"
+              type="password"
+              onChange={handleChange}
+              value={password}
+            />
+          </div>
+          <button type="submit" className={styles.btnPrimary}>Login</button>
+        </form>
+      </div>
+    </Layout>
+  );
+}
